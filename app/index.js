dbbe8698d11831ec9f6d4a609551972b887be86f
David <kadowki@gmail.com>
2014-07-28 11:10:46 -0500

    Added express web server and morgan.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@
 
 var express = require('express');
 var app     = express();
+var morgan  = require('morgan');
+
+//Good for logging stuff to the developer
+app.use(morgan('dev'));
 
 //Get takes two things
 //1: The path, 2: A callback function with two parameters: 
@@ -10,4 +14,6 @@ app.get('/', function(req, res){
 	res.send('We got it.');
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+	console.log('Express.js is listening on port 3000...');
+});
